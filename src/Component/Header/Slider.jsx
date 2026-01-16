89a649f0fe073e7639b3abc061b2a365961c2439
ahasana-ncs13 <ahasanancs0485@gmail.com>
2026-01-16 16:09:37 +0600

    Added CTA section in banner

--- src/Component/Header/Slider.jsx
+++ src/Component/Header/Slider.jsx
@@ -8,48 +8,67 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 // import required modules
-import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import { Autoplay, Pagination, Navigation, Parallax } from "swiper/modules";
+import { Link } from "react-router";
 
 const Slider = () => {
-
-    const [data , setData]=useState([])
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('https://import-export-hub-server-phi.vercel.app/slider')
-    .then(res=>res.json())
-    .then(data=>{
-        setData(data)
-        // console.log(data)   
-    })
+    fetch("https://import-export-hub-server-phi.vercel.app/slider")
+      .then((res) => res.json())
+      .then((data) => setData(data));
   }, []);
 
   return (
-    <>
-    <div className="w-11/12 mx-auto pt-26 my-10">
+    <div className="mt-6 md:mt-17 relative w-full h-[70vh] bg-lime-200 sm:h-[65vh] lg:h-[60vh]  overflow-hidden rounded-2xl">
       <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 3500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
+        autoplay={{ delay: 4000, disableOnInteraction: false }}
+        speed={800}
+        parallax={true}
+        pagination={{ clickable: true }}
         navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper"
+        modules={[Parallax, Pagination, Navigation, Autoplay]}
+        className="h-[60vh]"
       >
-        {
-            data.map(slide => <SwiperSlide key={slide._id}>
-                <h1 className="text-center text-xl mb-3 text-primary font-semibold">{slide.caption}</h1>
-                <img className="w-500 mx-auto h-140 rounded-2xl object-cover" src={slide.image_url} alt={slide.product} />
-            </SwiperSlide>) 
-        }
-        
+        {data.map((slide) => (
+          <SwiperSlide key={slide.id}>
+            <div className="grid grid-cols-1 lg:grid-cols-3 h-[60vh] items-center w-11/12 mx-auto gap-6 lg:gap-8">
+              {/* Text */}
+              <div
+                className="flex flex-col justify-center max-w-2xl p-4 sm:p-6 col-span-1"
+                data-swiper-parallax="-300"
+              >
+                <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold text-primary mb-3 sm:mb-4">
+                  {slide.product}
+                </h2>
+                <p className="text-gray-700 mb-4 sm:mb-6 text-sm sm:text-base">
+                  {slide.caption}
+                </p>
+                <Link
+                  to="/allproducts"
+                  className="inline-block bg-primary text-white px-5 sm:px-6 py-2.5 sm:py-3 rounded-lg shadow-lg hover:bg-secondary transition-all duration-300 w-fit"
+                >
+                  Import An Product
+                </Link>
+              </div>
+
+              {/* Image */}
+              <div className=" col-span-2">
+                <img
+                  src={slide.image_url}
+                  alt={slide.product}
+                  className="w-full max-w-sm sm:max-w-md lg:max-w-5xl h-38 sm:h-54 md:h-62 lg:h-[40vh] object-cover rounded-2xl shadow-lg"
+                  data-swiper-parallax="-100"
+                />
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
-      </div>
-    </>
+
+      <div className="absolute inset-0 bg-linear-to-b from-transparent to-white/20 pointer-events-none"></div>
+    </div>
   );
 };
 
